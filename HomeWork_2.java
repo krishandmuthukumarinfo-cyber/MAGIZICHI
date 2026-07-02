9a98eb6982c627f39322ca3b1cfa1749a3def575
unknown <you@example.com>
2026-07-02 16:09:18 +0530

    HomeWork_2

---
+++ HomeWork_2.java
@@ -0,0 +1,15 @@
+class HomeWork_2
+{
+	public static void main(String [] args)
+	{
+		String name = "HARI";
+		int age = 17;
+		char grade = 'b';
+		boolean pass = true;
+		
+		System.out.println("NAME ="+name);
+		System.out.println("AGE ="+age);
+		System.out.println("GRADE ="+grade);
+		System.out.println("RESULT ="+pass);
+	}
+}
