6d6bda8883d5d90d048985bf3af513e26023b90f
unknown <you@example.com>
2026-07-02 11:50:47 +0530

    HomeWork_1

---
+++ HomeWork_1.java
@@ -0,0 +1,10 @@
+class HomeWork_1
+{
+	public static void main(String [] args)
+	{
+		System.out.println("1.Dhoni finishes off in style!");
+		System.out.println("2.One strong filter coffee");
+		System.out.println("3.Balance: ₹10 Mic off, video off, sleeping ON!");
+		System.out.println("4.Ee sala cup namdhe");
+	}
+}
