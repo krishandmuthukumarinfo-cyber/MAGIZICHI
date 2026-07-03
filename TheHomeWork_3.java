a715bb3102b12ac837ad294b8945a8f969111b67
unknown <you@example.com>
2026-07-03 10:47:34 +0530

    this is variable test or homework

---
+++ TheHomeWork_3.java
@@ -0,0 +1,20 @@
+class TheHomeWork_3
+{
+	public static void main(String [] args)
+	{ 
+		byte mark1 = 80;
+		byte mark2 = 90;
+		byte mark3 = 70;
+		int total = mark1+ mark2 + mark3;
+		int average = total/3;
+		
+		System.out.println("MARK 1 = "+ mark1);
+		System.out.println("MARK 2 = "+ mark2);
+		System.out.println("MARK 3 = "+ mark3);
+		System.out.println("TOTAL = "+ total);
+		System.out.println("AVERAGE = "+ average);
+		
+		
+	
+	}
+}
