21d250a1188dd08254122c6cb96a8564c106838d
unknown <you@example.com>
2026-07-13 11:35:03 +0530

    this is nested loop project no 2

---
+++ Testing_3.java
@@ -0,0 +1,16 @@
+class Testing_2
+{
+	public static void main(String [] args)
+	{
+		//NESTED LOOPS PROJECT_2
+		for(int i=1;i<=5;i++)
+		{
+			for(char ch='A';ch<'A'+i;ch++)
+			{
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+	}
+		
+}
---
+++ Testing_4.java
@@ -0,0 +1,16 @@
+class Testing_2
+{
+	public static void main(String [] args)
+	{
+		//NESTED LOOPS PROJECT_2
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+		
+}
