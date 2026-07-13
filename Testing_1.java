19356e1dba68555a0f837f81366ff7755633cfaa
unknown <you@example.com>
2026-07-13 11:17:28 +0530

    this is nested loop project

---
+++ Testing_1.java
@@ -0,0 +1,16 @@
+class Testing_1
+{
+	public static void main(String [] args)
+	{
+		//NESTED LOOPS PROJECT _1
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+			
+	}
+}
